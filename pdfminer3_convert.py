f84bfe0935485e1b1b30b20eac06360a8f24f6a1
dhirlea <63645495+dhirlea@users.noreply.github.com>
2021-11-12 12:23:10 +0000

    added pdf to txt converter to help process large docs for similarity search

---
+++ pdfminer3_convert.py
@@ -0,0 +1,28 @@
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import PDFPageAggregator
+from pdfminer3.converter import TextConverter
+import io
+
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+with open('Teliasonera_2012.pdf', 'rb') as fh:
+
+    for page in PDFPage.get_pages(fh,
+                                  caching=True,
+                                  check_extractable=True):
+        page_interpreter.process_page(page)
+
+    text = fake_file_handle.getvalue()
+
+# close open handles
+converter.close()
+fake_file_handle.close()
+
+with open('example_converted_report.txt', 'w', encoding='utf-8') as f:
+    f.write(text)
