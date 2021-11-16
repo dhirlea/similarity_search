d91ff66b1f9a18b8dcdf3ca98f116faf3c2628f3
dhirlea <63645495+dhirlea@users.noreply.github.com>
2021-11-16 12:14:12 +0000

    cleaned up special characters

--- sentence_segmentation.py
+++ sentence_segmentation.py
@@ -1,5 +1,6 @@
 import spacy
 import json
+import re
 
 def main():
 
@@ -11,9 +12,16 @@ def main():
     doc = nlp(text_example)
 
     parsed_doc = {sent_no:sentence.text for sent_no, sentence in enumerate(doc.sents)}
+    cleaned_doc = {}
+    i = 0
+    for sent_no, sentence in parsed_doc.items():
+        cleaned_sentence = parsed_doc[sent_no].replace("\n","").replace("\f","").replace("\t","").replace("•","").strip()
+        if re.match("[a-zA-Z0-9]*", cleaned_sentence):
+            cleaned_doc[i] = cleaned_sentence
+            i +=1 
 
     with open('parsed_report.json','w',encoding='utf-8') as f:
-        f.write(json.dumps(parsed_doc, indent=4, ensure_ascii=False))
+        f.write(json.dumps(cleaned_doc, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
     main()
