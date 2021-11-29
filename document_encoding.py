289d3821b490fb6a77598e1afb14e19ac491c283
dhirlea <63645495+dhirlea@users.noreply.github.com>
2021-11-29 12:08:34 +0000

    added part of document encoding and clustering visualisation

--- document_encoding.py
+++ document_encoding.py
@@ -1,12 +1,13 @@
 from summarizer import Summarizer
 import torch
-import spacy
+import json
 import random
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
+from tqdm import tqdm
 
 def main():
     # Set random seeds
@@ -19,35 +20,13 @@ def main():
     else:
         device = torch.device("cpu")
 
-    body = "The Chrysler Building, the famous art deco New York skyscraper, will be sold for a small fraction of its previous sales price.\
-        The deal, first reported by The Real Deal, was for $150 million, according to a source familiar with the deal.\
-        Mubadala, an Abu Dhabi investment fund, purchased 90% of the building for $800 million in 2008.Real estate firm Tishman Speyer had owned the other 10%.\
-        The buyer is RFR Holding, a New York real estate company.\
-        Officials with Tishman and RFR did not immediately respond to a request for comments.\
-        It's unclear when the deal will close.\
-        The building sold fairly quickly after being publicly placed on the market only two months ago.\
-        The sale was handled by CBRE Group.\
-        The incentive to sell the building at such a huge loss was due to the soaring rent the owners pay to Cooper Union, a New York college, for the land under the building.\
-        The rent is rising from $7.75 million last year to $32.5 million this year to $41 million in 2028.\
-        Meantime, rents in the building itself are not rising nearly that fast.\
-        While the building is an iconic landmark in the New York skyline, it is ompeting against newer office towers with large floor-to-ceiling windows and all the modern amenities.\
-        Still the building is among the best known in the city, even to people who have never been to New York.\
-        It is famous for its triangle-shaped, vaulted windows worked into the stylized crown, along with its distinctive eagle gargoyles near the top.\
-        It has been featured prominently in many films, including Men in Black 3, Spider-Man, Armageddon, Two Weeks Notice and Independence Day.\
-        The previous sale took place just before the 2008 financial meltdown led to a plunge in real estate prices.\
-        Still there have been a number of high profile skyscrapers purchased for top dollar in recent years, including the Waldorf Astoria hotel, which Chinese firm Anbang Insurance purchased in 2016 for nearly $2 billion, and the Willis Tower in Chicago, which was formerly known as Sears Tower, once the world's tallest.\
-        Blackstone Group (BX) bought it for $1.3 billion 2015.\
-        The Chrysler Building was the headquarters of the American automaker until 1953, but it was named for and owned by Chrysler chief Walter Chrysler, not the company itself.\
-        Walter Chrysler had set out to build the tallest building in the world, a competition at that time with another Manhattan skyscraper under construction at 40 Wall Street at the south end of Manhattan. He kept secret the plans for the spire that would grace the top of the building, building it inside the structure and out of view of the public until 40 Wall Street was complete.\
-        Once the competitor could rise no higher, the spire of the Chrysler building was raised into view, giving it the title.\
-    "
-
-    nlp = spacy.load('en_core_web_sm')
-
-    doc = nlp(body)
-    sentence_list = []
-    for sentence in doc.sents:
-        sentence_list.append(sentence.text.strip())
+    with open('parsed_report.json', encoding='utf-8') as f:
+        body_dict = json.load(f)
+    
+    
+    raw_sentence_list = list(body_dict.values())
+    sentence_list = [sentence for sentence in raw_sentence_list[:100] if len(sentence)>1] #TODO only segmenting first 100 sentences. Extend to entire document
+    body = ' '.join(sentence_list)
 
     # Instantiate model
     model = Summarizer()
@@ -64,8 +43,8 @@ def main():
     # Encode individual sentences
 
     sentence_embeddings = []
-    for sentence in sentence_list:
-        sentence_embeddings.extend(model.run_embeddings(sentence, num_sentences=1, min_length=5)) # min length defaults to 40 chars to summarize text, but sentences can be shorter
+    for sentence in tqdm(sentence_list):
+        sentence_embeddings.extend(model.run_embeddings(sentence, num_sentences=1, min_length=1)) # min length defaults to 40 chars to summarize text, but sentences can be shorter
     
     sentence_embeddings_array  = np.array(sentence_embeddings)
 
